4c0e75c6aeeefa9760a07b36cb5c9ecfe3af9721
DESKTOP-4RBJS2K\Austin <austin.kwok@protonmail.com>
2023-02-13 16:57:26 -0800

    Update work experience

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -7,17 +7,37 @@ function WorkExperience() {
             <div id="Experience" className="container padding-60">
                 <h2 className="margin-top-20">Work Experience</h2>
                 <div className="row margin-top-20 justify-content-md-center work-cards">
+                <div className="col-lg-5 col-md-6 work-col">
+                        <div className="card work-card">
+                            <div>
+                                <img src="/D2L_Logo.jpg" class="card-img-top" alt="Luniu Mall"/>
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title text-center">D2L Corporation</h5>
+                                <p className="card-text text-center">Software Developer (Current)</p>
+                                <ul className="margin-top-20">
+                                    <li>Maintained and updated Quiz Creation and Quiz Taking for Learning Management System.</li>
+                                    <li>Built multiple web components and APIs for new quiz taking experience using C# and JavaScript.</li>
+                                    <li>Revamped the quiz timing system in preparation for synchronous quiz taking.</li>
+                                    <li>Wrote an integration that post build numbers to Github PRs and Rally stories during deployment phase. Saves
+88.75 work hours (organization wide).</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col-lg-5 col-md-6 work-col">
                         <div className="card work-card">
-                            <div className="padding-30">
-                                <img src="/luniu-mall.png" class="card-img-top" alt="UBC" />
+                            <div className="padding-20">
+                                <img src="/luniu-mall.png" class="card-img-top" alt="Luniu Mall" />
                             </div>
                             <div className="card-body">
                                 <h5 className="card-title text-center margin-top-20">Luniu Mall</h5>
-                                <p className="card-text text-center">Front End Developer (Current)</p>
+                                <p className="card-text text-center">Front End Developer</p>
                                 <ul className="margin-top-20">
-                                    <li>Worked on a Online Grocery Delivery app on IOS, Android, and Mobile Web Browsers (<a href="https://luniumall.com/">luniumall.com</a>).</li>
-                                    <li>Built numerous features such as User Reviews, User Posts, and Points Rewards System for the app.</li>
+                                    <li>Worked with a small team of 2-3 people on an Online Grocery Delivery app for IOS, Android, and Mobile Web
+Browsers. (<a href="https://luniumall.com/">luniumall.com</a>).</li>
+                                    <li>Designed and implemented numerous features such as User Reviews, User Posts and Points Rewards System
+for the app. Features were often made solo and are currently being used by 10k+ users.</li>
                                     <li>Utilized React Native, Expo, Android Studio, and XCode to produce apps for all mobile platforms.</li>
                                 </ul>
                             </div>
@@ -26,7 +46,7 @@ function WorkExperience() {
                     <div className="col-lg-5 col-md-6 work-col">
                         <div className="card work-card">
                             <div className="padding-20">
-                                <img src="/ffcb.png" class="card-img-top" alt="UBC" />
+                                <img src="/ffcb.png" class="card-img-top" alt="La Fédération des francophones de la Colombie-Britannique" />
                             </div>
                             <div className="card-body">
                                 <h5 className="card-title text-center">La Fédération des francophones de la Colombie-Britannique</h5>
@@ -54,7 +74,7 @@ function WorkExperience() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-5 col-md-6">
+                    {/* <div className="col-lg-5 col-md-6">
                         <div className="card work-card">
                             <img src="/cwh.jpg" className="card-img-top margin-top-20 margin-bottom-70 cwh-pic" alt="Canadian Web Hosting" />
                             <div className="card-body">
@@ -67,7 +87,7 @@ function WorkExperience() {
                                 </ul>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
