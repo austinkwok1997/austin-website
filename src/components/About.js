700b875a4f903ad1e52f45bcaf473fdf4f71387f
kwok8 <austin.kwok@protonmail.com>
2024-04-23 23:46:47 -0700

    Change description and resume

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,7 @@ function About() {
                 <div className="col-md-8 margin-top-20">
                     <div>
                         <h2 className="margin-top-20 about-me">About Me</h2>
-                        <p className="margin-top-20">Hi, I am a recent Grad from UBC who was born and raised in Vancouver, British Columbia. I majored in both Computer Science and Statistics which I believe go well together in areas such as Machine Learning and Algorithm Design.  Currently, I am looking for New Grad Software positions where I can refine my skills and grow as a Developer.</p>
-                        <p>Note: I am a CANADIAN CITIZEN who was born and raised in Vancouver, BC.</p>
+                        <p className="margin-top-20">Hi, my name is Austin and I'm a Software Developer with 3 years of experience. In 2020, I graduated from the University of British Columbia with a double major in Computer Science and Statistics. I currently work for D2L which is a Learning Management Software company. </p>
                         <a href="/Austin-Kwok-Resume.pdf" class="btn btn-info margin-top-20" role="button" aria-pressed="true">Resume</a>
                     </div>
                 </div>
