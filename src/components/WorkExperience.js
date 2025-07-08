97f75da2166d93da072ef3865e4fb01516054350
kwok8 <austin.kwok@protonmail.com>
2025-07-08 12:16:47 -0700

    update work experience

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -16,14 +16,13 @@ function WorkExperience() {
                                 <div className="col-lg-8 col-md-8">
                                     <div className="card-body">
                                         <h3 className="card-title">D2L Corporation</h3>
-                                        <p className="card-text">Software Developer 2 (Current)</p>
+                                        <p className="card-text">Software Engineer 2 (Current)</p>
                                         <ul className="margin-top-20">
-                                            <li>Led critical sections for the longstanding and highly requested automatic quiz submission feature from start to finish, which involved integration between both the instructor and student views and upkeep of backwards compatibility with legacy code. Since launch there has been an average of 2 million quiz attempts per week that uses this feature.</li>
-                                            <li>Supported and triaged quizzing related customer escalations on production to ensure SLAs of 99.9% uptime and tiered response times (ranging from 24 hours to 3 days) are prioritized and met accordingly.</li>
-                                            <li>Researched, created schemas, and wrote tests for an events-based system that generates quiz datasets for schools to analyze. This is part of an initiative that enables potential savings of up to $2.5 million per year.</li>
-                                            <li>Updated database model and schemas related to the quizzing tool’s time limit settings and performed the necessary migration steps on production to streamline the process for instructors to set time limits.</li>
-                                            <li>Wrote an integration that posts build numbers to Github PRs and Rally stories automatically during deployment phase, which saved the organization more than 86.75 work hours per month in manually tracking builds.</li>
-                                            <li>Built multiple web components and APIs for the new quiz editing experience on top of existing legacy code using C#, Lit/Lit 2.0, JavaScript, XML, and MSSQL.</li>
+                                            <li>Led critical sections for the longstanding and highly requested automatic quiz submission feature from start to finish, which involved integration between both the instructor and student views and upkeep of backwards compatibility with legacy code. Since launch there has been an average of 7 million quiz attempts per week that uses this feature.</li>
+                                            <li>Led the development of an Analytics Builder tool integrated with AWS QuickSight, enabling non-technical users to create custom data visualizations through a no-code interface and generative AI. I planned and prioritized user stories to align with product goals and technical feasibility.</li>
+                                            <li>Researched, created schemas, and wrote tests for an events-based system that ingests more than 10 million meta events per week to generate quiz datasets for school administrative and compliance requirements.</li>
+                                            <li>Updated database model and schemas and performed the necessary database migration steps on production to streamline the process for instructors to set time limits in the quizzing tool and enable synchronization of start and end times across all students taking a quiz, affecting more than 3.2 million students on a regular basis.</li>
+                                            <li>Wrote an integration as part of the CI/CD pipeline that posts build numbers to Github PRs and Rally stories automatically during deployment phase, which saved the organization more than 86.75 work hours per month in manually tracking builds.</li>
                                         </ul>
                                     </div>
                                 </div>
@@ -41,8 +40,9 @@ function WorkExperience() {
                                         <h3 className="card-title">Luniu Mall</h3>
                                         <p className="card-text">Front End Developer</p>
                                         <ul className="margin-top-20">
-                                            <li>Worked with a small team of 2-3 people on an Online Grocery Delivery app for iOS, Android, and Mobile Web Browsers. (<a href="https://luniumall.com/">luniumall.com</a>).</li>
-                                            <li>Designed and implemented numerous features on my own such as User Reviews, User Posts, and Points Rewards System for the app, which is currently being used by 10k+ users.</li>
+                                            <li>Worked with a small team of 2-3 people on a grocery delivery mobile app called Luniu Mall on both Apple iOS and Google Android: <a href="https://luniumall.com/app">https://luniumall.com/app</a>.</li>
+                                            <li>Designed and implemented numerous full stack features from scratch such as user reviews, user posts, and points rewards system for the app, which is currently being used by more than 10,000 users.</li>
+                                            <li>Improved load time on the product reviews page by 5 seconds through render optimizations made to the list component and implementing infinite scroll.</li>
                                             <li>Utilized React Native, Typescript, Expo, Android Studio, and XCode to produce apps for all mobile platforms.</li>
                                         </ul>
                                     </div>
@@ -61,8 +61,9 @@ function WorkExperience() {
                                         <h3 className="card-title">La Fédération des francophones de la Colombie-Britannique</h3>
                                         <p className="card-text">Junior Application Developer (Contract)</p>
                                         <ul className="margin-top-20">
-                                            <li>Built a Trivia Mobile Game for the Francophone community in BC using Unity, Strapi, and PostgresSQL.</li>
-                                            <li>Created backend RESTful API using Strapi, setup a PostgreSQL database, and built a “Wizard” application in React JS for non-technical employees to add trivia questions to the database.</li>
+                                            <li>Built a trivia mobile game called FrancoQuiz on both Apple iOS and Google Android for the French speaking community in BC which consists of 330,000 people: <a href="https://www.ffcb.ca/le-francoquiz/">https://www.ffcb.ca/le-francoquiz/</a></li>
+                                            <li>Built a wizard style web application that provides step-by-step guidance for non-technical employees to easily add trivia questions to the trivia question bank.</li>
+                                            <li>Used Unity for the mobile game and React JS for the web application, created a backend RESTful API using Strapi, and set up a PostgreSQL database which stored trivia questions and meta data.</li>
                                         </ul>
                                     </div>
                                 </div>
